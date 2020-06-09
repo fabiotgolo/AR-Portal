bf6c72e90a3ff66862ac0a0be32b27e59d3e1eaa
fabiotgolo <fabiotgarcia@olo.promo>
2020-06-09 16:15:03 -0300

    subindo assets

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+
+
+public class PlaneController : MonoBehaviour
+{
+    ARPlaneManager m_ARPlaneManager;
+
+    List<ARPlane> existingPlanes = new List<ARPlane>();
+
+    public Text buttonText;
+
+
+    private void Awake()
+    {
+        m_ARPlaneManager = GetComponent<ARPlaneManager>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void TogglePlaneDetectionAndVisibility()
+    {
+        m_ARPlaneManager.enabled = !m_ARPlaneManager.enabled;
+        if (m_ARPlaneManager.enabled)
+        {
+            SetAllPlanesActiveOrDeactive(true);
+            buttonText.text = "Disable Plane Detection and Hide Existing";
+        }
+        else
+        {
+            SetAllPlanesActiveOrDeactive(false);
+            buttonText.text = "Enable Plane Detection and Show Existing";
+
+        }
+
+
+
+    }
+
+
+    void SetAllPlanesActiveOrDeactive(bool value)
+    {
+        m_ARPlaneManager.GetAllPlanes(existingPlanes);
+
+        foreach (var plane in existingPlanes)
+        {
+            plane.gameObject.SetActive(value);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PortalPlacer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+public class PortalPlacer : MonoBehaviour
+{
+    ARSessionOrigin arSessionOrigin;
+
+    List<ARRaycastHit> raycast_hits = new List<ARRaycastHit>();
+
+
+
+    //this is the prefab to be instantiated
+    public GameObject PortalPrefab;
+
+    //this is the gameobject that is instantiated after successful raycast intersction with a plane
+    private GameObject spawnedPortal;
+
+
+
+    private void Awake()
+    {
+        arSessionOrigin = GetComponent<ARSessionOrigin>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount>0)
+        {
+
+            Touch touch = Input.GetTouch(0);
+
+            if (arSessionOrigin.Raycast(touch.position,raycast_hits,TrackableType.PlaneWithinPolygon))
+            {
+                //getting the pose of the hit
+                Pose pose = raycast_hits[0].pose;
+                if (spawnedPortal==null)
+                {
+                    spawnedPortal = Instantiate(PortalPrefab,pose.position,Quaternion.Euler(0,0,0));
+                    var rotationOfPortal = spawnedPortal.transform.rotation.eulerAngles;
+                    spawnedPortal.transform.eulerAngles = new Vector3(rotationOfPortal.x,Camera.main.transform.rotation.eulerAngles.y,rotationOfPortal.z);
+
+
+                }
+                else
+                {
+                    spawnedPortal.transform.position = pose.position;
+                    var rotationOfPortal = spawnedPortal.transform.rotation.eulerAngles;
+                    spawnedPortal.transform.eulerAngles = new Vector3(rotationOfPortal.x, Camera.main.transform.rotation.eulerAngles.y, rotationOfPortal.z);
+                }
+
+
+            }
+
+
+
+        }
+
+    }
+}
